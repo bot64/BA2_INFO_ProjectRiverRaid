4fe21818c40f31bea29319f3f75c72420ce69eeb
bot64 <minymerlin@gmail.com>
2024-05-05 18:54:00 +0200

    difficulty data class and main constants layed out

--- app/src/main/java/com/example/ba2_info_projectriverraid/utils/Constants.kt
+++
@@ -1,3 +0,0 @@
-package com.example.ba2_info_projectriverraid.utils
-
-val FUEL_DEPLETION_RATE : Double = 1.0
--- app/src/main/java/com/example/ba2_info_projectriverraid/utils/Difficulty.kt
+++ app/src/main/java/com/example/ba2_info_projectriverraid/utils/Difficulty.kt
@@ -1,32 +1,39 @@
 package com.example.ba2_info_projectriverraid.utils
 
 import com.example.ba2_info_projectriverraid.map.Boundary
+data class Map(
+    val mapId: Long,
+    val boundaries: List<Boundary>
+)
 
-// Difficulty.kt
-object Difficulty {
-
-    // Difficulty 000 settings (dev playground)
-    val DIFFICULTY_000_MAP_BOUNDARIES = listOf(
-        Boundary(0.0, 0.0, 100.0, 100.0)
+// Difficulty enum class
+enum class Difficulty(
+    val mapBoundaries: List<Boundary>,
+    val enemySpeed: Double = 1.0,
+    val enemyGenerationRate: Long,
+    val playerSpeed: Double = 1.0,
+    val playerStartingHealth: Int = 5) {
+    EASY(
+        mapBoundaries = listOf(Boundary(0.0, 0.0, 100.0, 100.0)),
+        enemySpeed = .75,
+        enemyGenerationRate = 75L,
+        playerStartingHealth = 10
+    ),
+    MEDIUM(
+        mapBoundaries = listOf(
+            Boundary(0.0, 0.0, 200.0, 200.0),
+            Boundary(50.0, 50.0, 150.0, 150.0)
+        ),
+        enemySpeed = 3.0,
+        enemyGenerationRate = 150L
+    ),
+    HARD(
+        mapBoundaries = listOf(
+            Boundary(0.0, 0.0, 300.0, 300.0),
+            Boundary(100.0, 100.0, 200.0, 200.0),
+            Boundary(150.0, 150.0, 250.0, 250.0)
+        ),
+        enemySpeed = 4.0,
+        enemyGenerationRate = 200L
     )
-    val DIFFICULTY_000_ENEMY_SPEED = 0.0
-    val DIFFICULTY_000_ENEMY_GENERATION_RATE = 0L
-
-    // Difficulty 001 settings (easy)
-    val DIFFICULTY_001_MAP_BOUNDARIES = listOf(
-        Boundary(0.0, 0.0, 100.0, 100.0)
-    )
-    val DIFFICULTY_001_ENEMY_SPEED = 2.0
-    val DIFFICULTY_001_ENEMY_GENERATION_RATE = 1000L
-
-    // Difficulty 002 settings (medium)
-    val DIFFICULTY_002_MAP_BOUNDARIES = listOf(
-        Boundary(0.0, 0.0, 200.0, 200.0),
-        Boundary(50.0, 50.0, 150.0, 150.0)
-    )
-    val DIFFICULTY_002_ENEMY_SPEED = 3.0
-    val DIFFICULTY_002_ENEMY_GENERATION_RATE = 750L
-
-    // ... and so on for other difficulty levels
-
 }
