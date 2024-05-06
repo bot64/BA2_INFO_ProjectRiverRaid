f65a6f35082ea5974997b2be7098b87001cf95f8
dmangen <167960910+dmangen@users.noreply.github.com>
2024-05-06 14:56:27 +0200

    minor changes

--- app/src/main/java/com/example/ba2_info_projectriverraid/entities/Entities.kt
+++ app/src/main/java/com/example/ba2_info_projectriverraid/entities/Entities.kt
@@ -7,19 +7,19 @@ import com.example.ba2_info_projectriverraid.entities.enemies.Ship
 
 
 abstract class Entities(
-    var x_pos: Float,
-    var y_pos: Float,
-    var size: Pair<Float, Float> = Pair(20.0f, 20.0f),
+    var entitiesX: Float,
+    var entitiesY: Float,
+    var entitiesSize: Pair<Float, Float> = Pair(20.0f, 20.0f),
     var health: Float,
     var image: Bitmap
 ){
     abstract val bitmap: Bitmap
     val data = Main.DifficultyDataManager.getData()
     fun getPosition() : Pair<Float, Float> {
-        return Pair(x_pos, y_pos)
+        return Pair(entitiesX, entitiesY)
     }
     fun isOutOfBounds(screenWidth : Int, screenHeight : Int) : Boolean {
-        return (x_pos < 0 || x_pos + size.first > screenWidth || y_pos < 0 || y_pos + size.second > screenHeight)
+        return (entitiesX < 0 || entitiesY + entitiesSize.first > screenWidth || entitiesY < 0 || entitiesY + entitiesSize.second > screenHeight)
     }
     fun remove(screenWidth : Int, screenHeight : Int, entities : MutableList<Entities>) {
         // Garbage collector based on outOfBounds()
@@ -32,35 +32,30 @@ abstract class Entities(
         entities.removeAll(entitiesToRemove)
     }
     fun isShot(missileX : Float, missileY : Float, missileSize : Pair<Float,Float>) : Boolean {
-        return x_pos - size.first / 2 < missileX + missileSize.first / 2 &&
-                x_pos + size.first / 2 > missileX - missileSize.first / 2 &&
-                y_pos - size.second / 2 < missileY + missileSize.second / 2 &&
-                y_pos + size.second / 2 > missileY - missileSize.second / 2
+        return entitiesX - entitiesSize.first / 2 < missileX + missileSize.first / 2 &&
+                entitiesX + entitiesSize.first / 2 > missileX - missileSize.first / 2 &&
+                entitiesY - entitiesSize.second / 2 < missileY + missileSize.second / 2 &&
+                entitiesY + entitiesSize.second / 2 > missileY - missileSize.second / 2
     }
     protected fun createEnemies(numEnemies : Int, entities : MutableList<Entities>, screenWidth : Int, screenHeight : Int) {
-        // Creates [numEnemies] 'enemies' objects at randomized (x_pos,y_pos) values
+        // Creates [numEnemies] 'enemies' objects at randomized (entitiesX,entitiesY) values
         repeat(numEnemies) {
             //val enemy = Ship(Random.nextFloat()*screenWidth, Random.nextFloat()*screenHeight, Pair(20f,20f))
             //entities.add(enemy)
         }
     }
     /*protected fun createBlocks(numBlocks : Int, entities : MutableList<Entities>, screenWidth : Int, screenHeight : Int) {
-        // Creates [numBlocks] 'block' objects at randomized (x_pos,y_pos) values
+        // Creates [numBlocks] 'block' objects at randomized (entitiesX,entitiesY) values
         repeat(numBlocks){
-            val block = Block(
-                Random.nextFloat() * screenWidth,
-                Random.nextFloat() * screenHeight
-            )
+            val block = Block(Random.nextFloat() * screenWidth,Random.nextFloat() * screenHeight)
             entities.add(block)
         }
     }
 
     protected fun createFuelTanks(numFuelTanks : Int, entities : MutableList<Entities>, screenWidth : Int, screenHeight : Int) {
-        // Creates [numFuelTanks] 'fuel_tank' objects at randomized (x_pos,y_pos) values
+        // Creates [numFuelTanks] 'fuel_tank' objects at randomized (entitiesX,entitiesY) values
         repeat(numFuelTanks){
-            val fuelTank = FuelTank(
-                Random.nextFloat()*screenWidth,
-                Random.nextFloat()*screenHeight)
+            val fuelTank = FuelTank(Random.nextFloat()*screenWidth,Random.nextFloat()*screenHeight)
             entities.add(fuelTank)
         }
     }
