241a5cd406d2020858f50ecbfa8e331acfad1377
bot64 <minymerlin@gmail.com>
2024-05-09 10:47:04 +0200

    update on activitymainxml

--- app/src/main/java/com/example/ba2_info_projectriverraid/MainActivity.kt
+++ app/src/main/java/com/example/ba2_info_projectriverraid/MainActivity.kt
@@ -9,6 +9,8 @@ import androidx.core.view.WindowInsetsCompat
 import com.example.ba2_info.projectriverraid.GameView
 import com.example.ba2_info_projectriverraid.utils.DifficultyAndData
 
+//teub
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
