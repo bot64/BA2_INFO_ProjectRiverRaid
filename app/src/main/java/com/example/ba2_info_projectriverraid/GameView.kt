3cc71f3dc912490eb84c4fb63ec58999d3806792
bot64 <minymerlin@gmail.com>
2024-05-09 19:03:26 +0200

    ship final mvp

--- app/src/main/java/com/example/ba2_info_projectriverraid/GameView.kt
+++ app/src/main/java/com/example/ba2_info_projectriverraid/GameView.kt
@@ -26,7 +26,7 @@ class GameView @JvmOverloads constructor (context: Context, attributes: Attribut
     var screenHeight = 0f
     var drawing = false
     lateinit var thread : Thread
-    val player = Player(entitiesX = width.toFloat()/2, entitiesY = height.toFloat()*0.8f, view = this)
+    val player = Player(0f,0f, view = this)
     val missile = Missile(player.entitiesX,player.entitiesY, view = this)
 
     init {
@@ -52,6 +52,7 @@ class GameView @JvmOverloads constructor (context: Context, attributes: Attribut
         super.onSizeChanged(w, h, oldw, oldh)
         screenWidth = w.toFloat()
         screenHeight = h.toFloat()
+        player.setPlayerXY(width.toFloat()/2, height.toFloat()*0.8f )
     }
 
 
