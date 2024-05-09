27031c3fbbcb95d74923a50c4d074147b57949ec
dmangen <david.mangen@ulb.be>
2024-05-09 12:03:22 +0200

    introducing coroutines to GameThread

--- app/src/main/java/com/example/ba2_info_projectriverraid/GameThread.kt
+++ app/src/main/java/com/example/ba2_info_projectriverraid/GameThread.kt
@@ -1,5 +1,6 @@
 import android.view.SurfaceHolder
 // import androidx.media3.player.Player
+import kotlinx.coroutines.*
 import android.content.Context
 import com.example.ba2_info_projectriverraid.entities.Block
 import com.example.ba2_info_projectriverraid.entities.Entities
@@ -10,27 +11,37 @@ import com.example.ba2_info_projectriverraid.GameView
 
 
 
-class GameThread(private val surfaceHolder: SurfaceHolder, private val gameView: GameView) : Thread() {
+class GameThread(private val surfaceHolder: SurfaceHolder, private val gameView: GameView){
     // Provides a factory method for creating Enemy, Block and FuelTank types of Entities at random coordinates
 
     // Game state variables
     var running = false
     private val context: Context = gameView.context
+    private val coroutineScope = CoroutineScope(Dispatchers.Default)
     //private val enemies = mutableListOf<Enemy>()
 
-    override fun run() {
-        while (running) {
-            // Update the game state
-            //player.update()
-            //enemies.forEach { it.update() }
-
-            // Draw the game objects
-            val canvas = surfaceHolder.lockCanvas()
-            if (canvas != null) {
-                gameView.draw(canvas)
-                surfaceHolder.unlockCanvasAndPost(canvas)
+    fun startGame(){
+        running = true
+        coroutineScope.launch{
+            while (running){
+                //Update the game state
+                //player.update()
+                //enemies.forEach{it.update()}
+                // Draw the game objects
+                withContext(Dispatchers.Main){
+                    val canvas = surfaceHolder.lockCanvas()
+                    if (canvas != null){
+                        gameView.draw(canvas)
+                        surfaceHolder.unlockCanvasAndPost(canvas)
+                    }
+                }
+                // Introduce delay() to control the game loop speed
             }
         }
+    }
+    fun stopGame(){
+        running = false
+        coroutineScope.cancel()
     }
 
     sealed class EntityType {
@@ -40,7 +51,7 @@ class GameThread(private val surfaceHolder: SurfaceHolder, private val gameView:
         data object FuelTank : EntityType()
     }
 
-    private fun createEntities(
+    private suspend fun createEntities(
         // Creating (numEntities) Entities after their 'type' parameter
         numEntities: Int, entities: MutableList<Entities>,
         screenWidth: Int, screenHeight: Int, type: EntityType
